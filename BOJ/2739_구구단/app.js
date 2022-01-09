be284eceec265d2407c60e59675d88d69ed32b43
yeopto <gunyeop530@gmail.com>
2022-01-09 14:40:01 +0900

    2022-01-09

---
+++ BOJ/2739_구구단/app.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+
+input = input[0];
+let input1 = Number(input);
+
+solution(input1);
+
+function solution(A) {
+    for (let i = 1; i < 10; i++) {
+        console.log(`${A} * ${i} = ${A * i}` );
+    }
+}
