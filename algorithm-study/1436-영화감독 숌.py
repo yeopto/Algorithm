c66963952137e4909cdd8dff130495602007517a
yeopto <gunyeop530@gmail.com>
2022-05-02 02:00:13 +0900

    2022-05-01

---
+++ algorithm-study/1436-영화감독 숌.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+cnt = 0
+num = 1
+while True:
+    if '666' in str(num):
+        cnt = cnt + 1
+    
+    if cnt == n:
+        print(num)
+        break
+    
+    num = num + 1
