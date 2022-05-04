363ad29b09ff5d0c9b51eea6b0b2f2ba3f5e42e8
yeopto <gunyeop530@gmail.com>
2022-05-04 13:38:54 +0900

    2022-05-04

---
+++ algorithm-study/11650-좌표 정렬하기.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = []
+
+for _ in range(N):
+    x, y = map(int, input().split())
+    arr.append([x, y])
+
+arr.sort()
+
+for i in arr:
+    print(*i)
