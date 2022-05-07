00ac81936a1419868c097a6f5db570563d4b396a
yeopto <gunyeop530@gmail.com>
2022-05-07 11:58:38 +0900

    2022-05-07

---
+++ algorithm-study/1934-최소공배수.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+n = int(input())
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    print(lcm(a, b))
